583bc28315ceaec54f8e9dfe4d73aa8ff8d5c03a
TUNMBI <adelowotumbi1@gmail.com>
2022-10-03 00:14:10 +0200

    pushing my 6th day experience

---
+++ script.js
@@ -0,0 +1,58 @@
+// functions
+
+
+// sum
+function sum(a, b){
+         console.log(12 + 4);
+     }
+     sum()
+document.getElementById('sum').innerHTML = "<code> Function sum (a , b) </code> <br> <code>  console.log(12 + 4) </code> <br> <code>  = " +(12 + 4) ;
+     
+     
+    //  minus
+     function minus(a, b){
+        console.log(10 - 5);
+    }
+    minus()
+document.getElementById('minus').innerHTML = "<code> Function minus (a , b) </code> <br> <code> console.log(10 - 5) </code> <br> <code> = " +(10 - 5) ;
+    
+   
+// multiply
+    function multiply(a, b){
+        console.log(4 * 4);
+    }
+    multiply()
+
+document.getElementById('multiply').innerHTML = "<code> Function multiply (a , b) </code> <br> <code>  console.log(4 * 4) </code> <br> <code>  = " + (4 * 4);
+    
+// division
+    function divide(a, b){
+        console.log(16 / 2);
+    }
+    divide()
+
+ document.getElementById('divide').innerHTML = "<code> Function divide (a , b) </code> <br> <code>  console.log(16 / 2) </code> <br> <code> = " + (16 / 2);
+    
+
+//  modulus
+    function modulus(a, b){
+        console.log(10 % 3);
+    }
+    modulus()
+
+document.getElementById('modulus').innerHTML = "<code> Function modulus (a , b) </code> <br> <code> console.log(10 % 3)  </code> <br> <code> = " + (10 % 3); 
+    
+// exponetial
+    function expo(a, b){
+        console.log(12 ** 4);
+    }
+    expo()
+document.getElementById('expo').innerHTML = "<code> Function expo (a , b) </code> <br> <code>   console.log(12 ** 4) </code> <br> <code>  = " + (12 ** 4);
+
+
+
+//  elements by id 
+document.getElementById('title6th').innerHTML="Assignment";
+document.getElementById('sixthday').innerHTML="<h2>My Sixth-day Javascript Experience</h2>";
+document.getElementById('experience6').innerHTML="My Sixth-Day experience in Javascript Was Also Complicated...My Sixth day lesson was on Javascript Functions..Javascript is fun to learn according to my research and I believe as times goes on and with a lot of practices i would Understand More better...";
+document.getElementById('subtopic').innerHTML="<h2>Javascript Functions</h2>";
